5f75759999e3a4b26d1664ecbd201c170b68c73b
dotnetcurry <dotnetcurry@a2zknowledgevisuals.com>
2012-05-07 10:13:01 +0530

    Removed HandleErrorAttribute()

--- FunWithSignalR.Web/Global.asax.cs
+++ FunWithSignalR.Web/Global.asax.cs
@@ -18,7 +18,7 @@ namespace FunWithSignalR
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            //filters.Add(new HandleErrorAttribute());
         }
 
         public static void RegisterRoutes(RouteCollection routes)
